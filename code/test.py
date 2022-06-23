e07f08d430c5c9d42f8855032142a2e2e099c642
alstjrdld1 <minseok_kim@khu.ac.kr>
2022-06-23 21:23:31 +0900

    20220623

--- code/test.py
+++ code/test.py
@@ -9,30 +9,15 @@ from UNSWGRAYDATASET import *
 
 from torch.utils.data import DataLoader
 
-if __name__ == "__main__":
-    pt_file = sys.argv[1]
-    print("Model load")
-    model = MobileNetV1(ch_in=1, n_classes=2)
-    print("Model load Complete")
-
+def test(model, test_loader, ptfile):
     print("Model weight load")
-    model.load_state_dict(torch.load('./ptfiles/' + pt_file))
+    model.load_state_dict(torch.load('./ptfiles/'+ptfile))
     model = model.cuda()
     print("Model weight load Complete")
-
-    print("loading test_data")
-    # test_data = MyDataSet_TEST()
-    # test_data = UNSWBINARYDATASETTEST()
-    # test_data = UNSWORIGINDATASETTEST()
-    test_data = UNSWGRAYDATASETTEST()
-    print("loading test_data complete")
-
-    test_loader = DataLoader(test_data, batch_size = 64, shuffle=False)
-    
     model.eval()
     
     correct = 0
-
+    Category = []
     print("Testing.....")
     for idx, (input, target) in enumerate(test_loader):
         input = np.array(input)
@@ -52,6 +37,7 @@ if __name__ == "__main__":
         # output = output.cpu().detach().numpy()
         output = torch.argmax(output, dim=1)
         output = output.cpu()
+        Category = Category + output.tolist()
 
         for i in range(len(output)):
 
@@ -59,8 +45,69 @@ if __name__ == "__main__":
                 correct += 1
                 tmp_correct += 1
         
-        print("Current acc => ", tmp_correct / len(output))
+        # print("Current acc => ", tmp_correct / len(output))
+    acc = correct/len(test_data)
+    print("==========================================")
+    print("Total Acc =>", acc)
+    print("==========================================")
+
+    df = pd.DataFrame(Category, columns=['Category'])
+    df.to_csv('./results/'+ptfile+'.csv', index=False)
+    print(ptfile, "done!")
+
+    return acc
+
+
+
+
+if __name__ == "__main__":
+    pt_file = sys.argv[1]
+    print("Model load")
+    model = MobileNetV1(ch_in=1, n_classes=2)
+    print("Model load Complete")
+
+    print("loading test_data")
+    # test_data = MyDataSet_TEST()
+    test_data = UNSWBINARYDATASETTEST()
+    # test_data = UNSWORIGINDATASETTEST()
+    # test_data = UNSWGRAYDATASETTEST()
+    print("loading test_data complete")
+
+    test_loader = DataLoader(test_data, batch_size = 64, shuffle=False)
     
-    print("==========================================")
-    print("Total Acc =>", correct/len(test_data))
-    print("==========================================")
+    pt_files = [
+        'binarytraining_9.pt',  'binarytraining_19.pt',
+        'binarytraining_29.pt', 'binarytraining_39.pt',
+        'binarytraining_49.pt', 'binarytraining_59.pt',
+        'binarytraining_69.pt', 'binarytraining_79.pt',
+        'binarytraining_89.pt', 'binarytraining_99.pt',
+        'binarytraining_109.pt','binarytraining_119.pt',
+        'binarytraining_129.pt','binarytraining_139.pt',
+        'binarytraining_149.pt'
+    ]
+    # pt_files = [
+    #     'graytraining_9.pt',    'graytraining_19.pt',
+    #     'graytraining_29.pt',   'graytraining_39.pt',
+    #     'graytraining_49.pt',   'graytraining_59.pt',
+    #     'graytraining_69.pt',   'graytraining_79.pt',
+    #     'graytraining_89.pt',   'graytraining_99.pt',
+    #     'graytraining_109.pt',  'graytraining_119.pt',
+    #     'graytraining_129.pt',  'graytraining_139.pt',
+    #     'graytraining_149.pt'
+    # ]
+    # pt_files = [
+    #     'originaltraining_9.pt',    'originaltraining_19.pt',
+    #     'originaltraining_29.pt',   'originaltraining_39.pt',
+    #     'originaltraining_49.pt',   'originaltraining_59.pt',
+    #     'originaltraining_69.pt',   'originaltraining_79.pt',
+    #     'originaltraining_89.pt',   'originaltraining_99.pt',
+    #     'originaltraining_109.pt',  'originaltraining_119.pt',
+    #     'originaltraining_129.pt',  'originaltraining_139.pt',
+    #     'originaltraining_149.pt'
+    # ]
+    test_acc_list = []
+    for ptfile in pt_files:
+        test_acc_list.append(test(model, test_loader, ptfile))
+    
+    test_acc_list = np.array(test_acc_list)
+    np.save("binary_test_acc_list", test_acc_list)
