219753f1b974f9b9c35209a3cbb5077263028ad0
alstjrdld1 <minseok_kim@khu.ac.kr>
2022-08-30 19:27:24 +0900

    20220830

--- code/test.py
+++ code/test.py
@@ -62,7 +62,7 @@ class AvalancheDataset(Dataset):
     def __init__(self):
         # data = pd.read_csv('./abnormals/ACK_Flooding_443.csv', index_col=False)
         data = pd.read_csv('./abnormals/UDP_Flooding.csv', index_col=False)
-        data = data.drop(['No.'], axis=1).values
+        data = data.drop(['No.', 'Info'], axis=1).values
         patches = []
         for dat in data:
             patches.append(make_patch(dat, (32, 32)))
